6be73cb71c1d8f825889154a3afa0916614fdf42
Wu Xinfeng <390155409@qq.com>
2022-10-12 03:42:54 +0000

    aaa

--- src/components/Menu/src/MenuCategoryItem.vue
+++ src/components/Menu/src/MenuCategoryItem.vue
@@ -1,18 +1,18 @@
 <template>
   <n-card :title="t(item.meta.title)" size="small">
-    <span>hehe</span>
+
 
   </n-card>
 
 </template>
 
 <script lang="ts" setup>
-import type {Menu} from '@/router/types';
+import type {AppRouteRecordRaw, Menu} from '@/router/types';
 import {NCard} from 'naive-ui';
 import {useI18n} from "@/hooks/web/useI18n";
 const {t}=useI18n();
 const props=defineProps({
-  item: Object as PropType<Nullable<Menu>>
+  item: Object as PropType<Nullable<AppRouteRecordRaw>>
 });
 
 </script>
