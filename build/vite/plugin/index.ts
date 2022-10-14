eae263185fe9ea20530a2d6eb5c213e7b50fb500
Wu Xinfeng <390155409@qq.com>
2022-10-14 12:00:59 +0000

    午饭

---
+++ build/vite/plugin/index.ts
@@ -0,0 +1,41 @@
+import { PluginOption } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueJsx from '@vitejs/plugin-vue-jsx';
+import VitePluginCertificate from 'vite-plugin-mkcert';
+import vueSetupExtend from 'vite-plugin-vue-setup-extend';
+import { configMockPlugin } from './mock';
+
+
+export function createVitePlugins(viteEnv: ViteEnv, isBuild: boolean) {
+    const {
+        VITE_USE_IMAGEMIN,
+        VITE_USE_MOCK,
+        VITE_LEGACY,
+        VITE_BUILD_COMPRESS,
+        VITE_BUILD_COMPRESS_DELETE_ORIGIN_FILE,
+    } = viteEnv;
+
+    const vitePlugins: (PluginOption | PluginOption[])[] = [
+        // have to
+        vue(),
+        // have to
+        vueJsx(),
+        // support name
+        vueSetupExtend(),
+        VitePluginCertificate({
+            source: 'coding',
+        }),
+    ];
+
+
+
+    // vite-plugin-mock
+    VITE_USE_MOCK && vitePlugins.push(configMockPlugin(isBuild));
+
+
+
+
+    return vitePlugins;
+
+
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,29 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import vue from '@vitejs/plugin-vue';
+import { loadEnv } from 'vite';
 import {resolve} from "path";
+import { wrapperEnv } from './build/utils';
+import {createVitePlugins} from "./build/vite/plugin";
 
 function pathResolve(dir: string) {
   return resolve(process.cwd(), '.', dir);
 }
 
+const root = process.cwd();
+
+const env = loadEnv(mode, root);
+
+// The boolean type read by loadEnv is a string. This function can be converted to boolean type
+const viteEnv = wrapperEnv(env);
+
+const { VITE_PORT, VITE_PUBLIC_PATH, VITE_PROXY, VITE_DROP_CONSOLE } = viteEnv;
+
+const isBuild = command === 'build';
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  // root:process.cwd(),
-  // base:'/',
-  plugins: [vue()],
+
+  plugins: createVitePlugins(viteEnv, isBuild),
   resolve: {
     alias: [
       {
